4fde8ecb4383ce5b284d0340f5bedfc7644e0f22
Paul Vaartjes <paul@pauls-macbook.lan>
2020-08-31 15:52:45 +1000

    First commit

---
+++ src/build/precache-manifest.fa08e5d0d83b32587c4e0df22778292a.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "bd2a3f093531878696450a332e51c70f",
+    "url": "/index.html"
+  },
+  {
+    "revision": "e7565fccf91f6380e8a4",
+    "url": "/static/css/main.4a9d094a.chunk.css"
+  },
+  {
+    "revision": "8520178d2c49134ee522",
+    "url": "/static/js/2.bc37484a.chunk.js"
+  },
+  {
+    "revision": "c64c486544348f10a6d6c716950bc223",
+    "url": "/static/js/2.bc37484a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "e7565fccf91f6380e8a4",
+    "url": "/static/js/main.584ab806.chunk.js"
+  },
+  {
+    "revision": "8931283fa7532b66ee7f",
+    "url": "/static/js/runtime-main.fca178c7.js"
+  }
+]);
